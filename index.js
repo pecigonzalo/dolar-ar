b8afd82a9261e58749ca981f40c456617e6e0483
Gonzalo Peci <pecigonzalo@users.noreply.github.com>
2020-05-14 20:11:58 +0200

    Add pino logging
    Also run prettier formatter.

--- index.js
+++ index.js
@@ -1,215 +1,232 @@
-let aws = require('aws-sdk');
+const pino = require("pino");
+const logger = pino({
+  prettyPrint: {
+    translateTime: true,
+    colorize: true,
+    ignore: "pid,hostname",
+  },
+});
+const aws = require("aws-sdk");
+const env = require("dotenv").config();
+const SlackWebhook = require("slack-webhook");
+const axios = require("axios");
 
-const env = require('dotenv').config();
-const SlackWebhook = require('slack-webhook');
 const slack = new SlackWebhook(process.env.SLACK_WEBHOOK || "abc");
-let currentRates = {};
 const tolerance = 0.001;
 const interval = process.env.INTERVAL || 60 * 1000; // 1 minute
-const axios = require('axios');
-let s3 = new aws.S3({
-  endpoint: process.env.S3_ENDPOINT || '',
+const s3 = new aws.S3({
+  endpoint: process.env.S3_ENDPOINT || "",
   s3ForcePathStyle: true,
 });
 
-let s3FileInfo = {
-  Bucket: process.env.STORE_BUCKET || '',
-  Key: process.env.STORE_KEY || ''
-}
+const s3FileInfo = {
+  Bucket: process.env.STORE_BUCKET || "",
+  Key: process.env.STORE_KEY || "",
+};
+
+let currentRates = {};
 
 // S3 Functions
 const loadStoredRates = (success, error) => {
-  console.log("Loading rates from S3")
+  logger.info("Loading rates from S3");
   s3.getObject(s3FileInfo, function (err, data) {
     if (err) {
       // TODO: handle this a bit better
       if (err.code === "NoSuchBucket") {
-        console.log("Bucket not found, creating");
-        var bucketParams = {
-          Bucket: s3FileInfo.Bucket
+        logger.warn("Bucket not found, creating");
+        let bucketParams = {
+          Bucket: s3FileInfo.Bucket,
         };
         s3.createBucket(bucketParams, function (err, data) {
           if (err) {
-            console.log("Error", err);
+            logger.error("Error", err);
           } else {
-            console.log("Created: ", data.Location);
+            logger.info("Created: ", data.Location);
             loadStoredRates();
           }
           error();
         });
       } else {
-        console.log("Error. Probably no initial rates found...", err);
+        logger.error("Error. Probably no initial rates found...", err);
         error();
       }
     } else {
-      currentRates = JSON.parse(data.Body.toString())
-      console.log("Previous rates found", currentRates);
+      currentRates = JSON.parse(data.Body.toString());
+      logger.info("Previous rates found", currentRates);
       success();
     }
-  })
-}
+  });
+};
 
 const saveRates = (callback) => {
-  console.log("Saving rates to S3", currentRates)
-  s3.putObject({
-    Bucket: s3FileInfo.Bucket,
-    Key: s3FileInfo.Key,
-    Body: JSON.stringify(currentRates),
-    ContentType: 'application/json'
-  }, function (error, response) {
-    if (error) console.error(error)
-    else console.log("Saved rates to S3")
-    if (callback) callback();
-  })
-}
-
+  logger.info("Saving rates to S3", currentRates);
+  s3.putObject(
+    {
+      Bucket: s3FileInfo.Bucket,
+      Key: s3FileInfo.Key,
+      Body: JSON.stringify(currentRates),
+      ContentType: "application/json",
+    },
+    function (error, response) {
+      if (error) console.error(error);
+      else logger.info("Saved rates to S3");
+      if (callback) callback();
+    }
+  );
+};
 
 const getDiff = (rate, currentRate) => {
-  return (rate / currentRate) - 1;
-}
+  return rate / currentRate - 1;
+};
 
 const getIcon = (diff) => {
-  return diff < 0 ?
-    '⬇️' :
-    diff === 0 ?
-    '🆗' :
-    '⬆️';
-}
+  return diff < 0 ? "⬇️" : diff === 0 ? "🆗" : "⬆️";
+};
 
 const sendToSlackChannel = (msg) => {
-  console.log("Sending message to Slack: " + msg)
-  if (process.env.NODE_ENV === 'production') {
+  logger.info(`Sending message to Slack: ${msg}`);
+  if (process.env.NODE_ENV === "production") {
     slack.send(msg);
   } else {
-    console.log(msg)
+    logger.info(msg);
   }
-}
+};
 
-const mapRates = _rates =>
+const mapRates = (_rates) =>
   _rates.reduce((rates, rate) => {
-    const {
-      compra,
-      venta
-    } = rate
+    const { compra, venta } = rate;
     rates[rate.key] = {
       compra,
-      venta
-    }
-    return rates
-  }, {})
+      venta,
+    };
+    return rates;
+  }, {});
 
 const updateRate = (rates) => {
-  console.log("Updating Rates");
-  let areAnyChanges = false
-  rates.forEach(rate => {
-    const currentRate = currentRates[rate.key]
-    const diffventa = getDiff(rate.venta, currentRate.venta)
-    const diffcompra = getDiff(rate.compra, currentRate.compra)
+  logger.info("Updating rates");
+  let areAnyChanges = false;
+  rates.forEach((rate) => {
+    const currentRate = currentRates[rate.key];
+    const diffventa = getDiff(rate.venta, currentRate.venta);
+    const diffcompra = getDiff(rate.compra, currentRate.compra);
     if (Math.abs(diffventa) >= tolerance || Math.abs(diffcompra) >= tolerance) {
-      areAnyChanges = true
-      const msg = `*${rate.name}:* Compra: ${getIcon(diffcompra)} 1 USD = *${rate.compra} ARS* - Venta: ${getIcon(diffventa)} 1 USD = *${rate.venta} ARS*`;
+      areAnyChanges = true;
+      const msg = `*${rate.name}:* Compra: ${getIcon(diffcompra)} 1 USD = *${
+        rate.compra
+      } ARS* - Venta: ${getIcon(diffventa)} 1 USD = *${rate.venta} ARS*`;
       sendToSlackChannel(msg);
     }
-  })
-  currentRates = mapRates(rates)
+  });
+  currentRates = mapRates(rates);
 
-  if (areAnyChanges) return new Promise(function (success) {
-    saveRates(success);
-  })
-  else console.log("All rates are the same. S3 not updated. No messages sent to Slack")
-}
+  if (areAnyChanges)
+    return new Promise(function (success) {
+      saveRates(success);
+    });
+  else
+    logger.info(
+      "All rates are the same. S3 not updated. No messages sent to Slack"
+    );
+};
 
-
-const getCronistaBNRate = url =>
+const getCronistaBNRate = (url) =>
   axios({
     url: "https://www.cronista.com/MercadosOnline/json/eccheader.json",
-    method: 'get'
-  }).then(response => {
-    const compra = +response.data.dolarbna.valorcompra
-    const venta = +response.data.dolarbna.valorventa
+    method: "get",
+  }).then((response) => {
+    const compra = +response.data.dolarbna.valorcompra;
+    const venta = +response.data.dolarbna.valorventa;
     return {
       compra,
-      venta
-    }
-  })
+      venta,
+    };
+  });
 
 const getCronistaBalanzRate = () =>
   axios({
     url: "https://www.cronista.com/_static_rankings/static_dolarbalanz.html",
-    method: 'get'
-  }).then(response => {
-    const compra = +response.data.Cotizacion.PrecioCompra
-    const venta = +response.data.Cotizacion.PrecioVenta
+    method: "get",
+  }).then((response) => {
+    const compra = +response.data.Cotizacion.PrecioCompra;
+    const venta = +response.data.Cotizacion.PrecioVenta;
     return {
       compra,
-      venta
-    }
-  })
+      venta,
+    };
+  });
 
 const getCronistaBlueRate = () =>
   axios({
-    url: "https://www.cronista.com/MercadosOnline/json/getValoresCalculadora.html",
-    method: 'get'
-  }).then(response => {
-    const blue = response.data.find(item => item.Id === 2)
-    const compra = +blue.Compra
-    const venta = +blue.Venta
+    url:
+      "https://www.cronista.com/MercadosOnline/json/getValoresCalculadora.html",
+    method: "get",
+  }).then((response) => {
+    const blue = response.data.find((item) => item.Id === 2);
+    const compra = +blue.Compra;
+    const venta = +blue.Venta;
     return {
       compra,
-      venta
-    }
-  })
+      venta,
+    };
+  });
 
-const rateMap = [{
-  key: "bna",
-  name: "BNA",
-  resolver: getCronistaBNRate
-}, {
-  key: "balanz",
-  name: "Balanz",
-  resolver: getCronistaBalanzRate
-}, {
-  key: "blue",
-  name: "Blue",
-  resolver: getCronistaBlueRate
-}]
+const rateMap = [
+  {
+    key: "bna",
+    name: "BNA",
+    resolver: getCronistaBNRate,
+  },
+  {
+    key: "balanz",
+    name: "Balanz",
+    resolver: getCronistaBalanzRate,
+  },
+  {
+    key: "blue",
+    name: "Blue",
+    resolver: getCronistaBlueRate,
+  },
+];
 
 const getRates = () =>
-  Promise.all(rateMap.map(
-    r => r.resolver().then(rate => ({
-      ...rate,
-      key: r.key,
-      name: r.name
-    }))
-  ))
+  Promise.all(
+    rateMap.map((r) =>
+      r.resolver().then((rate) => ({
+        ...rate,
+        key: r.key,
+        name: r.name,
+      }))
+    )
+  );
 
 const setInitialRate = () => {
-  return getRates()
-    .then(rates => {
-      currentRates = mapRates(rates)
-      console.log("Inital rates", currentRates);
-      return new Promise(function (success) {
-        saveRates(success)
-      })
-    })
-}
+  return getRates().then((rates) => {
+    currentRates = mapRates(rates);
+    logger.info("Inital rates", currentRates);
+    return new Promise(function (success) {
+      saveRates(success);
+    });
+  });
+};
 
 const loop = () => {
   let promise = new Promise(function (complete, failed) {
-    loadStoredRates(() => {
-        console.log("Initial rates loaded");
+    loadStoredRates(
+      () => {
+        logger.info("Loaded initial rates");
         getRates().then(updateRate);
       },
       () => {
-        console.log("No initial rates");
+        logger.info("No initial rates");
         setInitialRate();
-      })
-  })
-  return promise
-}
+      }
+    );
+  });
+  return promise;
+};
 
-console.log("Starting Interval Loop");
+logger.info(`Starting interval loop: ${interval}`);
 loop();
 setInterval(() => {
-  loop()
+  loop();
 }, interval);
